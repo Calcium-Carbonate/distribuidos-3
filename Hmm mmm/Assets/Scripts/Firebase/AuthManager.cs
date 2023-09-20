79f714c3e3ed4d1b69216d9d1375289521c51a5f
Camilo Correa Rojas <camilo.correar@upb.edu.co>
2023-09-19 23:15:19 -0500

    aa

---
+++ Hmm mmm/Assets/Scripts/Firebase/AuthManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using TMPro;
+
+
+
+public class AuthManager : MonoBehaviour
+{
+    //Firebase Variables
+    [Header(("Firebase"))]
+    public FirebaseAuth auth;
+    public FirebaseUser user;
+    
+    //Login variables
+    [Header(("Login"))]
+    public TMP_InputField emailLoginField;
+    public TMP_InputField passwordLoginField;
+    public TMP_Text warningLoginText;
+    public TMP_Text confirmLoginText;
+    
+    //Register variables
+    [Header(("Register"))] 
+    public TMP_InputField usernameRegisterField;
+    public TMP_InputField emailRegisterField;
+    public TMP_InputField passwordRegisterField;
+    public TMP_InputField passwordRegisterVerifyField;
+    public TMP_Text warningRegisterText;
+    
+
+
+
+}
