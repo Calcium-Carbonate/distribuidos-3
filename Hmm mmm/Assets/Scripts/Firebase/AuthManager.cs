ec00d11526a46cbb846fa7fcf6d64a32ed9fcc70
Camilo Correa Rojas <camilo.correar@upb.edu.co>
2023-09-20 00:12:29 -0500

    aa

--- Hmm mmm/Assets/Scripts/Firebase/AuthManager.cs
+++ Hmm mmm/Assets/Scripts/Firebase/AuthManager.cs
@@ -1,9 +1,9 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Firebase;
 using Firebase.Auth;
 using TMPro;
+using System.Threading.Tasks;
 
 
 
@@ -28,8 +28,43 @@ public class AuthManager : MonoBehaviour
     public TMP_InputField passwordRegisterField;
     public TMP_InputField passwordRegisterVerifyField;
     public TMP_Text warningRegisterText;
+
+    void Awake()
+    {
+        //Check that all of the necessary dependencies for Firebase are present on the system
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available)
+            {
+                //If they are avalible Initialize Firebase
+                InitializeFirebase();
+            }
+            else
+            {
+                Debug.LogError("Could not resolve all Firebase dependencies: " + dependencyStatus);
+            }
+        });
+    }
     
-
-
-
+    private void InitializeFirebase()
+    {
+        Debug.Log("Setting up Firebase Auth");
+        //Set the authentication instance object
+        auth = FirebaseAuth.DefaultInstance;
+    }
+    
+    //Function for the login button
+    public void LoginButton()
+    {
+        //Call the login coroutine passing the email and password
+        StartCoroutine(Login(emailLoginField.text, passwordLoginField.text));
+    }
+    //Function for the register button
+    public void RegisterButton()
+    {
+        //Call the register coroutine passing the email, password, and username
+        StartCoroutine(Register(emailRegisterField.text, passwordRegisterField.text, usernameRegisterField.text));
+    }
+    
 }
